881c210e238df7401c42bbfe47288177f6cb29c8
Caio <caiovr13@gmail.com>
2021-03-20 23:28:40 -0300

    Teste com UserDefaults

--- Annotations/ViewController.swift
+++ Annotations/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //UserDefaults.standard.set("testando", forKey: "teste")
+        //let text = UserDefaults.standard.object(forKey: "teste")
+        //print(text)
+        //UserDefaults.standard.removeObject(forKey: "teste")
     }
 
 
